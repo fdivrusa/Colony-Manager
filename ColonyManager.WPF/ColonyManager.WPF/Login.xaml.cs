451352b2426813fce0e01fdcf2f4a5c84b420a98
Di Vrusa Florian <florian.divrusa@realoldmen.com>
2021-01-22 14:01:02 +0100

    WPF base and login design

---
+++ ColonyManager.WPF/ColonyManager.WPF/Login.xaml.cs
@@ -0,0 +1,39 @@
+﻿using ColonyManager.Core;
+using System.Configuration;
+using System.Text;
+using System.Windows;
+
+namespace ColonyManager.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            InitConfiguration();
+        }
+
+        private void InitConfiguration()
+        {
+            StringBuilder errorMessage = new StringBuilder();
+            LocalSettings.ColonyManagerApiUrl = ConfigurationManager.AppSettings["ColonyManagerApiUrl"];
+
+            if (string.IsNullOrEmpty(LocalSettings.ColonyManagerApiUrl))
+            {
+                errorMessage.AppendLine("Cannot load ColonyManagerApiUrl value from configuration");
+            }
+
+            if (!string.IsNullOrEmpty(errorMessage.ToString()))
+            {
+                MessageBox.Show($"{errorMessage} \n Please contact your administrator", "Configuration error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
