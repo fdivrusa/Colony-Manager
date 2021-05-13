41b901c6e1946cd4564d6ea68f6949a6555a7da8
Di Vrusa Florian <florian.divrusa@gmail.com>
2021-05-13 22:31:22 +0200

    Align url for webApp

--- ColonyManager.WebApp/src/environments/environment.prod.ts
+++ ColonyManager.WebApp/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://colonymanager-webapi-host/api/',
+  apiUrl: 'https://localhos:5001/api/',
   appVersion: '1.0.0',
 };
--- ColonyManager.WebApp/src/environments/environment.ts
+++ ColonyManager.WebApp/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://colonymanager-webapi-host/api/',
+  apiUrl: 'https://localhos:5001/api/',
   appVersion: '1.0.0',
 };
 
